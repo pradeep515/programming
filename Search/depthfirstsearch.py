03ba52871037562d856397041c5e69ea20a33163
Pradeep <kalisetti@gmail.com>
2021-05-16 13:12:20 -0700

    DFS

---
+++ Search/depthfirstsearch.py
@@ -0,0 +1,18 @@
+# Using a Python dictionary to act as an adjacency list
+graph = {
+    'A' : ['B','C'],
+    'B' : ['D', 'E'],
+    'C' : ['F'],
+    'D' : [],
+    'E' : ['F'],
+    'F' : []
+}
+
+visted = set()
+
+def dfs(node, visited, graph):
+    if(node not in visted):
+        print (node)
+        visted.add(node)
+    for neighbour in graph[node]:
+        dfs(neighbour, visited, graph)
