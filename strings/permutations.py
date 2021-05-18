daca57b3a9f3abb21e10ebb3426d717dc6d70b2b
Pradeep <kalisetti@gmail.com>
2021-05-18 16:13:09 -0700

    permutation change

--- strings/permutations.py
+++ strings/permutations.py
@@ -1,14 +1,16 @@
 ## print out all the permutations of given string.
 
+def toString(List): ## just a utilty function to convert list to string
+    return ''.join(List)
 
 def permutate(name, l , r):
     if(l == r):
-        print(list)
+        print(toString(name))
     else:
         for i in range(l,r):
-            name[i],name[l] = name[l],name[i]
-            permutate(name, l+1, r)
-            name[i],name[l] = name[l],name[i] #backtracking
+            name[i],name[l] = name[l],name[i]  #swap values . A-> A , A->B and fix for that 
+            permutate(name, l+1, r)    # extend permutate to the remaining elements of the list.
+            name[i],name[l] = name[l],name[i] #backtracking  .  return state to the original position.
 
 if __name__ == '__main__':
     name = 'dory'
